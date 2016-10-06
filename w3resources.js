787ed28052d8a35e8e35e01bfe89826aea9492fe
= <dev@codeangler.com>
2016-10-06 08:54:00 -0600

    how would I use a purely functional application of #12?

--- w3resources.js
+++ w3resources.js
@@ -9,7 +9,25 @@
 // According to Wikipedia : In number theory, a perfect number is a positive integer that is equal to the sum of its proper positive divisors, that is, the sum of its positive divisors excluding the number itself (also known as its aliquot sum). Equivalently, a perfect number is a number that is half the sum of all of its positive divisors (including itself).
 // Example : The first perfect number is 6, because 1, 2, and 3 are its proper positive divisors, and 1 + 2 + 3 = 6. Equivalently, the number 6 is equal to half the sum of all its positive divisors: ( 1 + 2 + 3 + 6 ) / 2 = 6. The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
 
-()();
+let perfectNumber = (num) => {
+  let aliquotSum = [] // how would I generate an array with pure funcitonal programming
+  for(let i = 1; i < num; i ++){
+    if(num % i === 0){
+      aliquotSum.push(i)
+    }
+  }
+  console.log(aliquotSum.reduce((prev, current, i) => prev + current , 0));
+  if (aliquotSum.reduce((prev, current) => prev + current , 0) === num){
+    if ((aliquotSum.reduce((prev, current) => prev + current , 0) + num) / 2 === num){
+      return `you have a perfectNumber ${num}`
+    }
+  } else{
+    return false;
+  }
+}
+console.log(perfectNumber(6));
+
+// refactor idea with .filter()
 
 // 13. Write a JavaScript function to compute the factors of a positive integer. Go to the editor
 // Click me to see the solution.
